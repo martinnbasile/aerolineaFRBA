ceb89a99dcf79b546ae485c6a8cf67c72f343609
martinnbasilee <martinnbasile@gmail.com>
2015-09-15 21:47:35 -0300

    subo lo necesario para armar una base de pruebas

---
+++ Base de pruebas.sql
@@ -0,0 +1,25 @@
+use Pruebas
+
+create table roles (
+	id int primary key,
+	descri varchar(30)
+)
+
+create table Logins (
+	id int identity(1,1) primary key,
+	nombre varchar(30),
+	passEncrip varchar(256),
+	pass varchar(30)
+)
+
+
+insert into roles values (1, 'Admin')
+insert into roles values (2, 'Cliente')
+
+Create table usuarios (
+	id int identity(1,1) primary key,
+	username varchar(30),
+	preguntaSecreta varchar(40),
+)
+
+insert into usuarios values('martin','quien es puma?');
