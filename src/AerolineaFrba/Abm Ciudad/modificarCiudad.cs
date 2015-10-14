bcfc2009b1dcae20bd27c84012c5cc3ccd85c829
fede22 <fede192@hotmail.com>
2015-10-14 09:06:59 -0700

    Cambios en ABM Ciudades

--- src/AerolineaFrba/Abm Ciudad/modificarCiudad.cs
+++ src/AerolineaFrba/Abm Ciudad/modificarCiudad.cs
@@ -12,9 +12,27 @@ namespace AerolineaFrba.Abm_Ciudad
 {
     public partial class modificarCiudad : Form
     {
-        public modificarCiudad()
+        String ciudad;
+        public modificarCiudad(String unaCiudad)
         {
+            ciudad = unaCiudad;
             InitializeComponent();
+        }
+
+        private void modificarCiudad_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = ciudad;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            new buscarCiudad().Show();
+            this.Close();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
