4a9e457a010d7613db93a246ce0887a086de7c63
fede22 <fede192@hotmail.com>
2015-10-14 12:32:53 -0700

    Creada pantalla buscarAeronave

---
+++ src/AerolineaFrba/Abm Aeronave/buscarAeronave.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AerolineaFrba.Abm_Aeronave
+{
+    public partial class buscarAeronave : Form
+    {
+        public buscarAeronave()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            new Funcionalidades.Funcionalidades(Program.rol).Show();
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Validaciones.Validaciones.validarListBox(listBox1, "Selecciona una Aeronave a dar de baja por Vida Util"))
+            {
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Validaciones.Validaciones.validarListBox(listBox1, "Selecciona una Aeronave a dar de baja por Fuera de Servicio"))
+            {
+            }
+        }
+    }
+}
