37bd3e3a6ca11b8417e5b611538a592988d8a41e
fede22 <fede192@hotmail.com>
2015-10-15 09:01:49 -0700

    Detalles en bajaFueraDeServicio

--- src/AerolineaFrba/Abm Aeronave/bajaFueraDeServicio.cs
+++ src/AerolineaFrba/Abm Aeronave/bajaFueraDeServicio.cs
@@ -21,7 +21,7 @@ namespace AerolineaFrba.Abm_Aeronave
 
         private void bajaFueraDeServicio_Load(object sender, EventArgs e)
         {
-
+            textBox1.ReadOnly = true;
         }
 
         public void recibirFecha(DateTime unaFecha)
@@ -39,6 +39,21 @@ namespace AerolineaFrba.Abm_Aeronave
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Validaciones.Validaciones.validarTextBox(textBox1,"Seleccione una fecha"))
+            {
+              /*TODO Aca hay que validar si hay pasajes/encomiendas en el rango de fechas seleccionado
+              ;si hay aparece un messageBox en el que elije si quiere cancelar los pasajes/encomiendas
+              existentes o si quiere buscarle un reemplazo; si le da cancelar aparece un messageBox 
+              avisando que se cancelaron los pasajes y se hace lo correspondiente en la base para cancelarlos;
+              si elige buscar un reemplazo se chequea si hay alguna nave que cumpla las condiciones para reemplazarla;
+              si hay se abre una pantalla con una lista de Aeronaves que cumplen los parametros para reemplazar la actual (misma categoria, fabricante);
+              si no hay ninguna aparece un message box que dice "No hay aeronaves disponibles que puedan reemplazarla" o algo por el estilo y aparece una pantalla
+              para crear una nueva Aeronave*/
+            }
         }
     }
 }
