73c1b554dc0a9dfc18b16a43a769711194e74102
martinnbasilee <martinnbasile@gmail.com>
2015-11-07 20:05:21 -0300

    dalee

--- src/AerolineaFrba/Registro Llegada Destino/Registrar llegada a destino.cs
+++ src/AerolineaFrba/Registro Llegada Destino/Registrar llegada a destino.cs
@@ -25,9 +25,32 @@ namespace AerolineaFrba.Registro_Llegada_Destino
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+        {   
+            //Confirmar
             DateTime date=DateTime.Parse(dateTimePicker1.Value.TimeOfDay.ToString());
-            MessageBox.Show(date.ToString("HH:mm"));
+            String horaLlegada = date.ToString("HH:mm");
+            if (Validaciones.Validaciones.validarMatricula(maskedTextBox2,"Complete la matricula")){
+                String matricula = maskedTextBox2.Text;
+    
+            }
+            if (Validaciones.Validaciones.validarTextBox(textBox1, "Complete el aeropuerto de origen"))
+            {
+                String aeropuertoOrigen = textBox1.Text;
+                
+            }
+            if (Validaciones.Validaciones.validarTextBox(textBox2, "Complete el aeropuerto de destino"))
+            {
+                String aeropuertoDestino = textBox2.Text;
+                
+            }
+            //ConexionALaBase.Conexion.ejecutarNonQuery("exec 
+            MessageBox.Show("Falta pegarle a la base");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   //VOLVER
+            new Funcionalidades.Funcionalidades().Show();
+            this.Close();
         }
     }
 }
