7a5b4a396881e9ea242bc2c20eb8d3f81347357a
fede22 <fede192@hotmail.com>
2015-11-14 09:10:00 -0800

    Pantalla para canjear millas

--- src/AerolineaFrba/Canje Millas/MostrarInformacionMillas.cs
+++ src/AerolineaFrba/Canje Millas/MostrarInformacionMillas.cs
@@ -46,5 +46,25 @@ namespace AerolineaFrba.Canje_Millas
         {
 
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
