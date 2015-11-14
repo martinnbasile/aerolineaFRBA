adfc7e93391d4b1e75c0323c6ef30fa337c2ddfe
fede22 <fede192@hotmail.com>
2015-11-14 14:25:24 -0800

    Terminado canjear millas

--- src/AerolineaFrba/Canje Millas/canjeMillas.Designer.cs
+++ src/AerolineaFrba/Canje Millas/canjeMillas.Designer.cs
@@ -130,6 +130,7 @@
             this.button2.TabIndex = 38;
             this.button2.Text = "Confirmar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // canjeMillas
             // 
--- src/AerolineaFrba/Canje Millas/canjeMillas.cs
+++ src/AerolineaFrba/Canje Millas/canjeMillas.cs
@@ -13,6 +13,7 @@ namespace AerolineaFrba.Canje_Millas
     public partial class canjeMillas : Form
     {
         int dni;
+        int millasDisponibles;
 
         public canjeMillas(int unDni)
         {
@@ -22,7 +23,7 @@ namespace AerolineaFrba.Canje_Millas
 
         private void canjeMillas_Load(object sender, EventArgs e)
         {
-            ConexionALaBase.CargadorDeEstructuras.cargarDataGrid(dataGridView1, "select Descripcion,Millas_Necesarias from MM.Productos_Milla");
+            ConexionALaBase.CargadorDeEstructuras.cargarDataGrid(dataGridView1, "select Descripcion,Millas_Necesarias as 'Precio en millas' from MM.Productos_Milla");
             textBox1.Text = dni.ToString();
             System.Data.SqlClient.SqlDataReader reader = ConexionALaBase.Conexion.consultarBase("Select id from MM.clientes where DNI=" + dni);
             int numCliente;
@@ -30,7 +31,8 @@ namespace AerolineaFrba.Canje_Millas
             numCliente = (int)reader.GetSqlInt32(0);
             reader = ConexionALaBase.Conexion.consultarBase("select sum(millas) from MM.millas where cliente=" + numCliente);
             reader.Read();
-            textBox2.Text = (reader.GetSqlInt32(0)).ToString();
+            millasDisponibles = (int)reader.GetSqlInt32(0);
+            textBox2.Text = millasDisponibles.ToString();
 
         }
 
@@ -56,8 +58,33 @@ namespace AerolineaFrba.Canje_Millas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            new ingresarDniCanjeMillas();
+            new ingresarDniCanjeMillas().Show();
             this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Validaciones.Validaciones.validarDataGridView(dataGridView1, "Seleccione un producto"))
+            { if (Validaciones.Validaciones.validarNumericUpDown(numericUpDown1, "Seleccione una cantidad de productos a canjear"))
+              {
+                DataGridViewRow productoSeleccionado = this.dataGridView1.SelectedRows[0];
+                String descripcionProducto = productoSeleccionado.Cells["Descripcion"].Value.ToString();
+                int precioEnMillasPorUnidad = int.Parse(productoSeleccionado.Cells["Precio en millas"].Value.ToString());
+                int cantidadQueQuiereCanjear = Convert.ToInt32(numericUpDown1.Value);
+                 if ((cantidadQueQuiereCanjear * precioEnMillasPorUnidad )> millasDisponibles)
+                 {
+                     MessageBox.Show("No tiene las millas suficientes para realizar ese canje");
+                 }else
+                 {
+                        String noQuery = "exec MM.registrarCanje @dni="+dni+",@cantidad="+numericUpDown1.Value+",@descripcion='"+descripcionProducto+"'";
+                        ConexionALaBase.Conexion.ejecutarNonQuery(noQuery);
+                        MessageBox.Show("Se ha registrado la transaccion, debera ser aprobada por un administrador");
+                        new ingresarDniCanjeMillas().Show();
+                        this.Close();
+                 }
+                 
+               }   
+            }
         }
     }
 }
